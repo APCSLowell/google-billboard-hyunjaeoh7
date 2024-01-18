574dbeaf9fa0e15729855d5c71d1b2245c408193
hyunjaeoh7 <143122764+hyunjaeoh7@users.noreply.github.com>
2024-01-18 11:47:58 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -3,6 +3,10 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
+		for(int i = 0; i < args.length(); i++){
+			String dig = args.substring(i,i+10);
+			double bruh = Double.parseDouble(dig);
+		}
 		System.out.println("Replace this string with your answer!");
 	}
 	
