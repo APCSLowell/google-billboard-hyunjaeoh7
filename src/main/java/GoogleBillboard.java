506e89dea11da56e89829eec04180dd00a1649c4
hyunjaeoh7 <143122764+hyunjaeoh7@users.noreply.github.com>
2024-01-19 11:04:57 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,35 +1,29 @@
+public final static String e = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public void setup()  
+{            
+  for(int i = 0; i < e.length()-10; i++){
+    String dig = e.substring(i,i+10);
+    double bruh = Double.parseDouble(dig);
+    if(isPrime(bruh)){
+      System.out.println(dig);
+      break;
+    }
+  }
+}  
 
-
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		for(int i = 0; i < args.length(); i++){
-			String dig = args.substring(i,i+10);
-			double bruh = Double.parseDouble(dig);
-			if(isPrime(bruh)){
-				System.out.println(dig);
-				return "";
-			}
-		}
-	}
-	
-
-	//Finish this function
-	public boolean isPrime(double num){
-		if(num < 2){
-    			return false;
-  		}
-  		if(num != 2 && num % 2 == 0){
-    			return false;
-  		}
-  
-  		for(int i = 1; i <= Math.sqrt(num); i+=2){
-   	 		if(num % i == 0 && i != 1){
-      				return false;
-    			}
-  		}
-  		return true;
-		}
-
-	}
-
+public boolean isPrime(double num)  
+{   
+    if(num < 2){
+      return false;
+    }
+    if(num != 2 && num % 2 == 0){
+      return false;
+    }
+    
+    for(int i = 1; i <= Math.sqrt(num); i+=2){
+      if(num % i == 0 && i != 1){
+        return false;
+      }
+    }
+    return true;  
+} 
