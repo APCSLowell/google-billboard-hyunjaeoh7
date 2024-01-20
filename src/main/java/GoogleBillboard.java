1c1592b4649408a9fad21d7c6f3b9094b6df51fc
hyunjaeoh7 <143122764+hyunjaeoh7@users.noreply.github.com>
2024-01-19 19:18:44 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -17,6 +17,9 @@ public class GoogleBillboard{
       if(num < 2){
         return false;
       }
+      if(num != (int)num){
+        return false;
+      }
       for(int i = 2; i <= Math.sqrt(num); i++){
         if(num % i == 0){
           return false;
