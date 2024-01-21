c3b8cd64b9e306e1e79028cd598cf26d6d63d791
hyunjaeoh7 <143122764+hyunjaeoh7@users.noreply.github.com>
2024-01-20 22:44:35 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,4 +1,4 @@
-public final static String e = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 public void setup()  
   {            
     noLoop();
@@ -15,6 +15,9 @@ public void setup()
 public boolean isPrime(double num)  
   {   
       if(num < 2){
+        return false;
+      }
+      if(num < 5){
         return false;
       }
       for(int i = 2; i <= Math.sqrt(num); i++){
